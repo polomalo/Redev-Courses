5ddb53b831100cd1d5eea4d1478f57b31640d562
Dmitry <dima7012095@gmail.com>
2025-03-20 21:45:47 +0300

    first and second task

--- props-task/src/App.jsx
+++ props-task/src/App.jsx
@@ -1,11 +1,45 @@
-import { useState } from 'react'
 import './App.css'
+
+function Num(props) {
+  return <h1>Number: {props.number}</h1>
+}
+
+function Str(props) {
+  return <h1>String: {props.string}</h1>
+}
+
+function Boolean(props) {
+  return <h1>Boolean: {props.boolean ? 'true' : 'false'}</h1>
+}
+
+function Obj(props) {
+  return <h1>Object:<br/> First name - {props.obj.firstName}<br/> Last name - {props.obj.lastName}</h1>
+}
+
+function Func(props) {
+  return <button onClick={props.onClick}>Click me</button>
+}
+
+function Arr(props) {
+  return (
+    <ul>
+      {props.todos.map((item, index) => (
+        <li key = {index}>{item}</li>
+      ))}
+    </ul>
+  )
+}
 
 function App() {
 
   return (
     <>
-      ddddddddddddd
+      <Num number={777}/>
+      <Str string={'Dima Kirkitsky'}/>
+      <Boolean boolean={false}/>
+      <Obj obj={{firstName: 'Dima', lastName: 'Kirkitsky'}}/>
+      <Func onClick={() => alert('Good job!')}/>
+      <Arr todos={['Wake up', 'Bathroom', 'Eat', 'Work', 'Study', 'Sleep']}/>
     </>
   )
 }
