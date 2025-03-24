253723d63c7cff59c7cd374f1aa26edb26c999fd
Dmitry <dima7012095@gmail.com>
2025-03-24 20:52:14 +0300

    state tasks

--- props-task/src/App.jsx
+++ props-task/src/App.jsx
@@ -1,46 +1,60 @@
 import './App.css'
+import React, { useState } from 'react'
 
-function Num({number}) {
-  return <h1>Number: {number}</h1>
-}
-
-function Str({string}) {
-  return <h1>String: {string}</h1>
-}
-
-function Boolean({boolean}) {
-  return <h1>Boolean: {boolean ? 'true' : 'false'}</h1>
-}
-
-function Obj(props) {
-  const {firstName, lastName} = props.obj
-  return <h1>Object:<br/> First name - {firstName}<br/> Last name - {lastName}</h1>
-}
-
-function Func(props) {
-  return <button onClick={props.onClick}>Click me</button>
-}
-
-function Arr(props) {
+function IncreaseCount() {
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    setCount(count => count + 1)
+  }
   return (
-    <ul>
-      {props.todos.map((item, index) => (
-        <li key = {index}>{item}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>First Task</h2>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increase</button>
+    </div>
+  )
+}
+
+function HideText() {
+  const [showText, setShowText] = useState(true)
+  const handleClick = () => {
+    setShowText(showText => !showText)
+  }
+  return (
+    <div>
+      <h2>Second Task</h2>
+      <button onClick={handleClick}>Hide</button>
+      {showText && <p>Hide this text!</p>}
+    </div>
+  )
+}
+
+function ChangeColorText() {
+  const [colorText, setColorText] = useState()
+  const handleClick = () => {
+    setColorText(
+      colorText =>
+        (colorText = `#${Math.floor(Math.random() * 16777215).toString(16)}`)
+    )
+  }
+  const style = {
+    color: colorText,
+  }
+  return (
+    <div>
+      <h2>Third Task</h2>
+      <button onClick={handleClick}>Change color</button>
+      <p style={style}>Change my color!</p>
+    </div>
   )
 }
 
 function App() {
-
   return (
     <>
-      <Num number={777}/>
-      <Str string={'Dima Kirkitsky'}/>
-      <Boolean boolean={false}/>
-      <Obj obj={{firstName: 'Dima', lastName: 'Kirkitsky'}}/>
-      <Func onClick={() => alert('Good job!')}/>
-      <Arr todos={['Wake up', 'Bathroom', 'Eat', 'Work', 'Study', 'Sleep']}/>
+      <IncreaseCount />
+      <HideText />
+      <ChangeColorText />
     </>
   )
 }
